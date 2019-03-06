62e9917a18379367dcb9d5cea6aa60cc6f86268a
Mathias Prisfeldt <contact@mathiasprisfeldt.me>
2019-03-06 21:00:23 +0100

    Added more test dat

--- src/main/kotlin/me/mathiasprisfeldt/blog/configurations/BlogConfiguration.kt
+++ src/main/kotlin/me/mathiasprisfeldt/blog/configurations/BlogConfiguration.kt
@@ -17,16 +17,40 @@ class BlogConfiguration {
     @Bean
     fun databaseIntializer(userRepository: UserRepository,
                            articleRepository: ArticleRepository) = ApplicationRunner {
-        val user = User("admin", "password", "Admin", "Jensen")
-        user.description = "Server Administrator"
-        userRepository.save(user)
+        val user1 = User("admin", "password", "Admin", "Jensen")
+        user1.description = "Server Administrator"
+        userRepository.save(user1)
+
+        val user2 = User("Tonni Bonde", "password", "Tonni", "Bonde")
+        user2.description = "Bedste Salgsmand 1984"
+        userRepository.save(user2)
+
+        val user3 = User("Micki Lynge", "password", "Micki", "Lynge")
+        user3.description = "Kan sit rullepølse kram"
+        userRepository.save(user3)
 
         articleRepository.save(Article(
                 title = "Hvordan du gør mit job lettere.",
                 headline = "Få en masse visninger",
                 content = "Egentlig burde der står en masse gejl om hvordan man gør en server" +
                         "administrator jobs nemmere, men kunne ikke finde på noget.",
-                author = user
+                author = user1
+        ))
+
+        articleRepository.save(Article(
+                title = "Hvordan flygter i en fart.",
+                headline = "Der er ikke tid til headlines",
+                content = "Køb dig nogle hurtige sko og briller og kom så afsted, den bliver" +
+                        "ikke sværere.",
+                author = user2
+        ))
+
+        articleRepository.save(Article(
+                title = "Hvordan du tilbereder en rullepølse.",
+                headline = "Mere salt",
+                content = "Sørg for at kødet ikke er blevet for gammelt, og så lidt mere salt." +
+                        "Husk er smage på det, og så noget mere salt.",
+                author = user3
         ))
     }
 }
